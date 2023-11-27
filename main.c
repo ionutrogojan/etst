a4127e8889fd2071c0ca961f1b9b47a0eec0256e
ionutrogojan <i.rogojan15@gmail.com>
2023-11-27 16:08:38 +0000

    cross-platform config path

--- main.c
+++ main.c
@@ -3,19 +3,26 @@
 #include <string.h>
 
 #define MAX_LINK_LENGTH 2000
-#define MAX_PATH_LENGTH 4096
 
-int openLink(const char* link) {
+#ifdef __linux__ // LINUX
+	#define MAX_PATH_LENGTH 4096
+#elif __APPLE__ // MACOS
+	#define MAX_PATH_LENGTH 256
+#elif _WIN32 // WINDOWS
+	#define MAX_PATH_LENGTH 260
+#endif
+
+int openLink(const char *link) {
 	// xdg-open + https://www... + > /dev/null 2>&1 + spaces & '\0'
 	// 8 + 2000 + 16 + 3 (rounded to 2048)
-	char cmd[2048];
+	char cmd[MAX_LINK_LENGTH + 48];
 	int fmtRet = 0;
 #ifdef __linux__ // LINUX
-	fmtRet = sprintf(cmd, "xdg-open '%s' > /dev/null 2>&1", link);
+	fmtRet = snprintf(cmd, sizeof(cmd), "xdg-open '%s' > /dev/null 2>&1", link);
 #elif __APPLE__ // MACOS
-	fmtRet = sprintf(cmd, "open '%s'", link);
+	fmtRet = snprintf(cmd, sizeof(cmd), "open '%s'", link);
 #elif _WIN32 // WINDOWS
-	fmtRet = sprintf(cmd, "explorer '%s'", link);
+	fmtRet = snprintf(cmd, sizeof(cmd), "explorer '%s'", link);
 #endif
 	if (fmtRet < 0 || fmtRet > sizeof(cmd)) {
 		printf("  [ERROR] Unable to format 'openLink' command\n");
@@ -25,28 +32,59 @@ int openLink(const char* link) {
 	return ret;
 }
 
+const char *getConfigDirectory(void) {
+
+#ifdef _WIN32 // WINDOWS
+	const char *appData = getenv("APPDATA");
+	if (appData == NULL)
+		return NULL; // Error: APPDATA environment variable not found
+	return appData;
+#endif
+
+	static char configPath[MAX_PATH_LENGTH]; // static buffer to avoid dealloc
+
+	const char *homeDir = getenv("HOME");
+	if (homeDir == NULL)
+		return NULL; // Error: HOME environment variable not found
+
+#ifdef __linux__ // LINUX	
+	int fmtRet = snprintf(configPath, sizeof(configPath), "%s/.config", homeDir);
+#elif __APPLE__ // MACOS
+	int fmtRet = snprintf(configPath, sizeof(configPath), "%s/Library/Preferences", homeDir);
+#endif
+
+	if (fmtRet < 0 || fmtRet > sizeof(configPath))
+		return NULL; // Error: Formatting failed or buffer overflow
+
+	return configPath;
+}
+
 int main(void) {
-	FILE* file = NULL;
-	char line[MAX_LINK_LENGTH];
-	char configPath[MAX_PATH_LENGTH]; // Linux MAX_PATH_LENGTH is 4096 chars / bytes
 
-	const char* homeDir = getenv("HOME");
+#ifdef _WIN32 // WINDOWS
+	char divider = '\\';
+#else // UNIX
+	char divider = '/';
+#endif
 
-	if (homeDir != NULL) {
-		// TODO: find a way to format the path better
-		// homeDir + /.config/webbox/ + configFile
-		int fmtRet = sprintf(configPath, "%s/.config/webbox/default.cfg", homeDir);
-		if (fmtRet < 0 || fmtRet > sizeof(configPath)) {
-			printf("  [ERROR] Unable to format 'configPath'\n");
-			return 1;
-		}
+	char configPath[MAX_PATH_LENGTH];
+	const char *configDir = getConfigDirectory();
+
+	int fmtRet = snprintf(configPath, sizeof(configPath), "%s%cwebbox%cdefault.cfg", configDir, divider, divider);
+	if (fmtRet < 0 || fmtRet > sizeof(configPath)) {
+		printf("  [ERROR] Unable to format 'configPath'\n");
+		return 1;
 	}
+
+	FILE *file = NULL;
 
 	if ((file = fopen(configPath, "r")) == NULL) {
 		printf("  [ERROR] Unable to read '%s'\n", configPath);
 		return 1;
 	}
-	
+
+	char line[MAX_LINK_LENGTH];
+
 	while (fgets(line, sizeof(line), file) != NULL) {
 		switch (line[0]) {
 			case '\n':
--- readme.md
+++ readme.md
@@ -10,12 +10,16 @@ A complete rewrite of all previous versions. Quickly launch a browser with a lis
 
 This is a free and open source application. In order to get it on your system follow the build instructions
 
+## Prerequisites
+- `GCC` or a C compiler of your choice
+- `make` to run the automatic build setup
+
 ## Build Instructions
 
 The following Build Instructions are for `Linux`, the `MacOS` build should be identical and if you're a `Windows` user, may god help you to set up a C compiler.
 
 ```C
-!IMPORTANT! Make sure you have GCC installed on your system
+!IMPORTANT! Make sure you have a C compiler installed on your system
 // feel free to modify the 'makefile' and use a different compiler
 
 // the default build is a static build. It will compile all the libraries and code into an executable with no dependencies
@@ -25,11 +29,17 @@ make
 // to make a shared executable which will not compile all the libraries and will be dependent on your system's libC, run the following command:
 make shared
 ```
-P.S. I just realized I hardcoded the config path and it will probably work only on Linux, so change it to your config path for the moment until I get to that. Sorry for the inconvenience
 
 Once the program is built there's one more step.
-Create a folder in your `.config/` folder called webbox and place the `test.cfg` file in there. Your final result should be something like this:
+
+Create a directory in your `~/.config/` (LINUX) , `~/Library/Preferences/` (MACOS) or `~\AppData\Roaming\` (WINDOWS) directory called webbox and place the `example.cfg` file in there.
+
+Copy the file, don't move it permanently in case you might need an example for later use.
+
+Your final result should be something like this:
+```C
+/home/<username>/.config/webbox/example.cfg					// LINUX
+/Users/<Username>/Library/Preferences/webbox/example.cfg	// MACOS
+/C:\Users\YourUsername\AppData\Roaming\webbox\example.cfg	// WINDOWS
 ```
-../home/<username>/.config/webbox/test.cfg
-```
-Webbox will look for a `default.cfg` so rename `test.cfg` to that and modify the links to your desired ones.
+Webbox will look for a `default.cfg` so rename `example.cfg` to that and modify the links to your desired ones or create a new file and add your links there following the example file.
